a56473e690579e34177022d4bd5211ad8d8d4ef0
Kerby-cpu <charmeskerby@gmail.com>
2024-12-19 20:33:51 -0500

    Update session.js

--- session.js
+++ session.js
@@ -52,5 +52,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID ANJU-MD=qJ8EQR5Y#q1ePMgBglh1KrfjxU3OrLdxcsghbgbyeLfpEYnXnndY "your session_id here",
 };
